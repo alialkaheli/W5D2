a9abbf68d37795191fa29cc694feb0928e64affc
ali alkaheli <alialkaheli@alis-mbp.attlocal.net>
2018-11-20 18:04:04 -0800

    set up the routes

--- redditClone/config/routes.rb
+++ redditClone/config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resource :session, only: [:new,:create, :destroy]
+
+  resources :subs, except: [:destroy]
+
+  resources :posts, except: [:destroy, :index] do
+    resources :comments, only: [:new]
+  end
+
+  resources :comments, only: [:create, :show]
 end
